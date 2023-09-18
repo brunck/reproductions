e3fe9dc422dcd7dd4ed268a538371d77fabb00f3
Brian Runck <brunck@intermatic.com>
2023-09-18 13:02:25 -0400

    Added .NET MAUI build error - Resource Dictionary

---
+++ MauiResourceDictionaryBuildError/MauiRc1App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiRc1App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiResourceDictionaryBuildError/MauiRc1App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiRc1App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
