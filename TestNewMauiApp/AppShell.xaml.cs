e0788d19a424f58383e01221f7f618d6bfa5cb56
Brian Runck <brunck@intermatic.com>
2025-01-27 15:12:14 -0500

    For testing

---
+++ TestNewMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNewMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestNewMauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestNewMauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
